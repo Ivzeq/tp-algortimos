378170ba32c733774cc7b67619200bdece68ece3
Mob <118850824+Mob-h@users.noreply.github.com>
2024-10-21 14:15:43 -0300

    Update and rename programa.py to GUIA.py

---
+++ src/GUIA.py
@@ -0,0 +1,7 @@
+"""
+PERFILES
+1.cliente  #diagrama de actividades //CLIENTE//
+2.administrador #diagrama de actividades //empleado.administrador(*2)//
+3.cocinero #diagrama de actividades //empleado.cocinero//
+4.mesero  #diagrama de actividades //empleado.mesero// Y //empleado.recepcionista//
+"""
